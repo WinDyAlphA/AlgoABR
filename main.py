edee0c2611488cb54d1c4c7ce9d78f97b55dd585
WinDyAlphA <heraud260@gmail.com>
2023-01-15 20:57:59 +0100

    suppr gui

--- main.py
+++ main.py
@@ -8,6 +8,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import customtkinter as ctk
 
+
+def mainSuppr(val):
+    val = int(val)
+    
+    ctk.set_appearance_mode("dark") 
+    ctk.set_default_color_theme("dark-blue")
+
+    root = ctk.CTk()
+    root.geometry("500x200")
+    root.title("Rechercher")
+
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+    if (recherche(arbre,val)):
+        label = ctk.CTkLabel(master=frame, text="Le nombre a bien été supprimé")
+        frame1 = ctk.CTkFrame(master=frame,
+                               width=50,
+                               height=50,
+                               corner_radius=10,
+                               fg_color="green")
+        delete_val(arbre,val)
+        
+    else:
+        label = ctk.CTkLabel(master=frame, text="Le nombre n'existe pas")
+        frame1 = ctk.CTkFrame(master=frame,
+                               width=50,
+                               height=50,
+                               corner_radius=10,
+                               fg_color="red")
+    label.pack(pady=12, padx=10)
+    frame1.pack(pady=12, padx=10)
+    root.mainloop()
+
+def mainSupprimer():
+    ctk.set_appearance_mode("dark") 
+    ctk.set_default_color_theme("dark-blue")
+
+    root = ctk.CTk()
+    root.geometry("500x200")
+    root.title("Rechercher")
+
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+    entry = ctk.CTkEntry(master=frame, placeholder_text="Entrer la valeur a rechercher")
+    entry.pack(pady=12, padx=10)
+    btn4 = ctk.CTkButton(master=frame, text="Supprimer", command=lambda:mainSuppr(entry.get()))
+    btn4.pack(pady=12, padx=10)
+    root.mainloop()
+
 def mainRechercheViz(arbre,val):
     val = int(val)
     if (recherche(arbre,val)):
@@ -146,17 +195,20 @@ def app():
     label = ctk.CTkLabel(master=frame, text="Bienvenue dans l'application de visualisation d'arbre binaire de recherche")
     label.pack(pady=12, padx=10)
 
-    btn = ctk.CTkButton(master=frame, text="voir la liste", command=mainListe)
+    btn = ctk.CTkButton(master=frame, text="Voir la liste", command=mainListe)
     btn.place(x=100, y=50)
 
-    btn2 = ctk.CTkButton(master=frame, text="trier la liste grace a l'arbre", command=mainTrier)
+    btn2 = ctk.CTkButton(master=frame, text="Trier la liste grace a l'arbre", command=mainTrier)
     btn2.place(x=250, y=50)
 
-    btn1 = ctk.CTkButton(master=frame, text="visualiser l'arbre", command=mainVisualisation)
+    btn1 = ctk.CTkButton(master=frame, text="Visualiser l'arbre", command=mainVisualisation)
     btn1.place(x=175, y=100)
 
-    btn3 = ctk.CTkButton(master=frame, text="rechercher", command=mainRechercher)
+    btn3 = ctk.CTkButton(master=frame, text="Rechercher", command=mainRechercher)
     btn3.place(x=175, y=150)
+
+    btn4 = ctk.CTkButton(master=frame, text="Supprimer", command=mainSupprimer)
+    btn4.place(x=175, y=200)
 
 
 
--- suppression.py
+++ suppression.py
@@ -20,3 +20,4 @@ def find_min(node):
     while current.left:
         current = current.left
     return current.val
+
