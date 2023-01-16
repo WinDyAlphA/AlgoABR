b4e6300ecd4378b420bc2eb9b0e9910c3b12f750
NoahHeraud <40216866+WinDyAlphA@users.noreply.github.com>
2023-01-16 14:08:44 +0000

    Pending changes exported from your codespace

--- generateur.py
+++ generateur.py
@@ -1,14 +1,40 @@
 import random
 import csv
 
+
 def generateur(n):
     liste = []
     for i in range(n):
-        liste.append(random.randint(0,100))
+        liste.append(random.randint(0, 100))
     return liste
+
 
 def csvwrite(liste):
     with open('ABRcsv.csv', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(liste)
-    
+
+
+def csvadd(val):
+    val = str(val)
+    with open('ABRcsv.csv', 'a', newline='') as csv_file:
+        csv_file.write(f",{val}")
+
+def csvdel(val):
+    val = str(val)
+    with open("ABRcsv.csv", "r") as file:
+        reader = csv.reader(file)
+        lines = []
+        found = False
+        for line in reader:
+            new_line = []
+            for x in line:
+                if x == val and not found:
+                    found = True
+                    continue
+                new_line.append(x)
+            lines.append(new_line)
+
+    with open("ABRcsv.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerows(lines)
